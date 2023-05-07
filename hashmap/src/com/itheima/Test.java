ce8050e5478e22f956995b81847b1c4780557619
eniac555 <1150304664@qq.com>
2023-05-07 21:25:37 +0800

    hashMap源码分析

---
+++ hashmap/src/com/itheima/Test.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author eniac555
+ * @date 2023/5/7
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(10 & 8);
+        System.out.println(2 & 8);
+        System.out.println(18 & 8);
+        System.out.println(26 & 8);
+    }
+}
